05852e36b49e22aafbb4f6ea1f1cf56425813659
wyoikawa <william.oikawa@gmail.com>
2017-05-10 23:16:26 -0300

    ajustes html 10/05

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -1,7 +1,9 @@
 var cssId = 'myCss';
+
 if (!document.getElementById(cssId)) {
 	var head  = document.getElementsByTagName('head')[0];
 	var link  = document.createElement('link');
+	link.content = "text/css";
 	link.rel  = 'stylesheet';
 	link.type = 'text/css';
 	link.href = 'https://raw.githubusercontent.com/wyoikawa/teste_pmweb/master/prod/css/styles.min.css';
